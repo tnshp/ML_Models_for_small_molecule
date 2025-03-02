47358da025f7e363521833ad26648bb475ae1cc5
tnshp <phopalkartanish@iitgn.ac.in>
2025-03-03 02:59:41 +0530

    --

--- SchNet/main.py
+++ SchNet/main.py
@@ -31,6 +31,7 @@ def parse_args():
     parser.add_argument("--model_save_path", type=str, default="trained_model.pth", help="Path to save the trained model")
     parser.add_argument("--num_train", type=int, default=1000, help="Number of samples to use for training")
     parser.add_argument("--gpus", type=int, default=0, help="Number of GPUs to use (-1 for all available)")
+    parser.add_argument("--early_stopping", action="store_true", help="enable early stopping")
     return parser.parse_args()
 
 def main(args):
@@ -119,8 +120,11 @@ def main(args):
             mode="min",
             save_top_k=1
         ),
-        EarlyStopping(monitor="val_loss", mode="min")
     ]
+    if args.early_stopping:
+        callbacks.append(
+            EarlyStopping(monitor="val_loss", mode="min", patience=5)
+        )
 
     # Determine GPU usage
     if args.gpus == -1:
