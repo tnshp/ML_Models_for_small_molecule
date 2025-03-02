fixed energy units

SchNet/main.py
@@ -97,19 +97,19 @@ def main(args):
     output_forces = spk.task.ModelOutput(
         name='forces',
         loss_fn=torch.nn.MSELoss(),
-        loss_weight=0.5,
+        loss_weight=0.7,
         metrics={"MAE": torchmetrics.MeanAbsoluteError()}
     )
     output_energy = spk.task.ModelOutput(
         name='energy',
         loss_fn=torch.nn.MSELoss(),
-        loss_weight=0.5,
+        loss_weight=0.3,
         metrics={"MAE": torchmetrics.MeanAbsoluteError()}
     )
 
     task = spk.task.AtomisticTask(
         model=nnpot,
-        outputs=[output_forces, output_energy],
+        outputs=[output_energy, output_forces],
         optimizer_cls=torch.optim.AdamW,
         optimizer_args={"lr": args.lr}
     )
@@ -146,7 +146,7 @@ def main(args):
         accelerator=accelerator,
         devices=devices,
         strategy='ddp' if devices and devices > 1 else 'auto',  # Use DDP for multi-GPU training
-        enable_progress_bar=False
+        enable_progress_bar=True
     )
     
 

SchNet/xyz_npz.py
@@ -63,14 +63,21 @@ parser.add_argument("-n","--npz_file_path", type=str, help="npz file path")
 # Parse arguments
 args = parser.parse_args()
 
-
+ 
 # Parse the XYZ file
 positions, atomic_numbers, forces, energies = parse_xyz_file(args.xyz_file_path)
+energies = energies / 4184 #joule to kcal
 
 # print(forces)
-# print(energies)
+# print(energies) 
 
 # Save data to NPZ file
+print(energies[0:5])
+# print(forces[0:5])
+print(forces.mean())
+print(positions[0:5])
+print(atomic_numbers[0:5])
+
 np.savez(args.npz_file_path, positions=positions, atomic_numbers=atomic_numbers, forces=forces, energies=energies)
 
 print(f"Data saved to {args.npz_file_path}")